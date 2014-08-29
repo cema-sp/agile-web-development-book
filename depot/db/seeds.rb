2b0017fd21b8fe1a3297575c5d6124adf96b183c
cema-sp <cema.rus@gmail.com>
2014-08-29 10:50:55 +0400

    seeds file added

--- depot/db/seeds.rb
+++ depot/db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+
+Product.create(title: 'Jasmine Oolong Tea',
+				description: %{
+					This most precious of green oolong teas 
+					is made more delicate with the gentle 
+					scenting of fragrantly sweet jasmine. 
+					Creating a hint of perfumed wonder, this 
+					sublime and aromatic hand-rolled tea is 
+					nothing less than a cup of transcendental 
+					bliss. 
+					},
+				image_url: 'jasmine.jpeg',
+				price: 12.98)
+Product.create(title: 'Four Seasons Oolong Tea',
+				description: %{
+					Refreshing and fun, this rolled oolong 
+					hails from the renowned Nantou region 
+					of Taiwan. Known locally as Si Ji Chun, 
+					meaning "four seasons," this full-bodied 
+					cup is golden amber in color and pleasantly 
+					aromatic, evoking sweet jasmine, lily, 
+					and honey. Mellow and fruity with a sweet 
+					buttery finish, you will enjoy the delicious 
+					taste day or night. 
+					},
+				image_url: 'fours.jpeg',
+				price: 17.98)
+Product.create(title: 'Toasted Nut Brulee Oolong Tea',
+				description: %{
+					Savor each sweet sip as this silky modern 
+					brûlée channels the decadent scenery of 
+					the French Riviera. This sinless oolong 
+					tea aims to capture a sweet, sovereign dessert 
+					bursting with coconut, cinnamon and caramel. 
+					},
+				image_url: 'toasted.jpeg',
+				price: 7.98)
