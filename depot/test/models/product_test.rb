99334389c20056f8b24d3bf250a4f78a45b14916
cema-sp <cema.rus@gmail.com>
2014-08-29 12:23:32 +0400

    added some tests

--- depot/test/models/product_test.rb
+++ depot/test/models/product_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "product attributes must not be empty" do
+		product = Product.new
+		assert product.invalid?
+		assert product.errors[:title].any?
+		assert product.errors[:description].any?
+		assert product.errors[:price].any?
+		assert product.errors[:image_url].any?
+	end
+	test "product price must be positive" do
+		product = Product.new(title: 'Test 02',
+								description: 'Test 02',
+								image_url: '123.gif')
+		product.price = -1
+		assert product.invalid?
+		assert_equal "must be greater than or equal to 0.01",
+						product.errors[:price].join('; ')
+
+		product.price = 0.001
+		assert product.invalid?
+		assert_equal "must be greater than or equal to 0.01",
+						product.errors[:price].join('; ')
+		product.price = 0.01
+		assert product.valid?
+	end
+	test "" do
+		def new_product(image_url)
+			Product.new(title: 'Test 03',
+						description: 'Test 03',
+						price: 1,
+						image_url: image_url)
+		end
+
+		ok = %w{ fred.gif fred.jpg fred.PNG fReD.jPg http://one.two/a/b/c/fred.GIF}
+		not_ok = %w{ fred.doc fred.gif/one fred.gif.more }
+
+		ok.each do |name|
+			assert new_product(name).valid?, "#{name} should be valid"
+		end
+		not_ok.each do |name|
+			assert new_product(name).invalid?, "#{name} shouldn't be valid"
+		end
+	end
 end
