be449f0c917efe4d0edea2b28eb6d6c9923c34ed
cema-sp <cema.rus@gmail.com>
2014-08-30 22:33:44 +0400

    sessions counter to side panel

--- depot/app/controllers/store_controller.rb
+++ depot/app/controllers/store_controller.rb
@@ -1,5 +1,13 @@
 class StoreController < ApplicationController
   def index
   	@products = Product.order(:title)
+
+  	if session[:counter].nil?
+  		@session_counter = 1
+  	else
+  		@session_counter = session[:counter].to_i + 1
+  	end
+  	session[:counter] = @session_counter
+  	 			
   end
 end
