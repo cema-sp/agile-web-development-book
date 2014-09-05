649e558073bce0ba3b4dcba93732aac1f809b874
cema-sp <cema.rus@gmail.com>
2014-09-05 16:15:12 +0400

    new Users resource with BCrypt passwords

--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+
   resources :orders do
     get :ship, on: :member
   end
---
+++ depot/test/controllers/users_controller_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionController::TestCase
+  setup do
+    @user = users(:cema)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:users)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create user" do
+    assert_difference('User.count') do
+      post :create, user: { name: 'Test_User', password: 'pass', password_confirmation: 'pass' }
+    end
+
+    assert_redirected_to users_path
+  end
+
+  test "should show user" do
+    get :show, id: @user
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @user
+    assert_response :success
+  end
+
+  test "should update user" do
+    patch :update, id: @user, user: { name: 'Cema_', password: 'pass_', password_confirmation: 'pass_' }
+    assert_redirected_to users_path
+  end
+
+  test "should destroy user" do
+    assert_difference('User.count', -1) do
+      delete :destroy, id: @user
+    end
+
+    assert_redirected_to users_path
+  end
+end
